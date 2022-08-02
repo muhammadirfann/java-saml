ddde5f8ce9ba1e2723ad38847d33cced66bc323d
Muhammad Irfan <irfan697@gmail.com>
2022-08-02 12:02:23 -0400

    ESYNC-5618 - Jakarta transformed to support Java9+ ecosystem

--- toolkit/src/test/java/com/onelogin/saml2/test/AuthTest.java
+++ toolkit/src/test/java/com/onelogin/saml2/test/AuthTest.java
@@ -36,9 +36,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import org.joda.time.Instant;
 import org.junit.Rule;
